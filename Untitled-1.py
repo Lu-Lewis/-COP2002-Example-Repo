656e367a2d07c880eb456f617fb8f07c746ee305
Lu-Lewis <helloluke@gmail.com>
2024-05-19 18:04:07 -0400

    Feature.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,4 @@
-print("Luke Lewis")
-print("COP2002.0M2")
-print("Summer 2024")
-print("This is an interactive session!")
+# Creating a project for GitHub
+# By Luke Lewis
 
-
+print("COP2002 Summer 2024")
